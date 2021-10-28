848acc52bac3e2a19167457b23056432a3da28d4
David Mihal <david@dmihal.com>
2021-10-28 09:14:06 +0100

    Add CORS headers

--- pages/api/daily-fees/[date].ts
+++ pages/api/daily-fees/[date].ts
@@ -23,6 +23,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     })
     const json = await apiRequest.json()
 
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if (req.method == 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+      return res.status(200).json({})
+    }
+
     if (json?.data?.solana?.totalFees) {
       res.setHeader('Cache-Control', 'max-age=60, s-maxage=${60 * 60}, stale-while-revalidate');
       res.json({ statusCode: 200, value: json?.data?.solana?.totalFees })
