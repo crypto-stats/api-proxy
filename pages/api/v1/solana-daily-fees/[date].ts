bf589f036dfedebb9f20676ac2bc981110830c27
David Mihal <david@dmihal.com>
2022-07-15 19:36:23 +0100

    Update solana endpoint

--- pages/api/v1/solana-daily-fees/[date].ts
+++ pages/api/v1/solana-daily-fees/[date].ts
@@ -1,55 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-
-const ONE_DAY = 24 * 60 * 60 * 1000
+import { wrapHandler } from 'utils/requests'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const startDate = new Date(req.query.date as string)
-    const endDate = new Date(startDate.getTime() + ONE_DAY - 1)
+  const dateReversed = (req.query.date as string).split('-').reverse().join('-')
+  const apiRequest = await fetch(`https://hyper.solana.fm/v3/tx-fees?date=${dateReversed}`)
 
-    const apiRequest = await fetch('https://api.solana.fm/', {
-      headers: {
-        apikey: process.env.SOLANAFM_KEY,
-        'Content-Type': "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({
-        query: `{
-          solana {
-            totalFeesInTimeRange(time: { 
-              from: "${startDate.toISOString()}", 
-              to: "${endDate.toISOString()}", 
-              resolution: ONE_DAY}) {
-              time,
-              value
-            }
-          }
-        }`
-      }),
-    })
-    const json = await apiRequest.json()
+  // The endpoint is currently returning an incorrect content-type. Once fixed, this check should be reenabled
+  // if (!apiRequest.headers.get('Content-Type').startsWith('application/json')) {
+  //   const value = await apiRequest.text()
+  //   throw new Error(`Invalid response: ${value}`)
+  // }
 
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-    if (req.method == 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-      return res.status(200).json({})
-    }
+  const json = await apiRequest.json()
 
-    if ((json?.data?.solana?.totalFeesInTimeRange || []).length > 0) {
-      res.setHeader('Cache-Control', 'max-age=60, s-maxage=${60 * 60}, stale-while-revalidate');
-      res.json({ statusCode: 200, value: json?.data?.solana?.totalFeesInTimeRange[0].value })
-      return
-    }
-
-    res.status(500).json({ statusCode: 404, message: `Couldn't find data on ${req.query.date}`, error: json.errors })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ statusCode: 500, message: err.message })
+  if (json.total_tx_fees) {
+    res.setHeader('Cache-Control', `max-age=${60 * 60}, s-maxage=${24 * 60 * 60}, stale-while-revalidate`);
+    res.json({ success: true, value: json.total_tx_fees })
+    return
   }
+  console.warn(json)
+
+  res.status(404).json({ success: false, error: `Couldn't find data on ${req.query.date}` })
 }
 
-export default handler
+export default wrapHandler(handler)
