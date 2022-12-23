3e1559971499a829f9f3e6cc2b3dcdece4d9511a
Zach Miller <home@mac-2.local>
2022-12-23 10:51:59 -0500

    added cache to zapper endpoint

--- pages/api/v1/zapper/[addresses].ts
+++ pages/api/v1/zapper/[addresses].ts
@@ -1,15 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { wrapHandler } from 'utils/requests'
-import EventSource from 'eventsource'
+import { NextApiRequest, NextApiResponse } from "next";
+import { wrapHandler } from "utils/requests";
+import EventSource from "eventsource";
+import { CryptoStatsSDK, Context } from "@cryptostats/sdk";
 
 const generateUrl = (addresses: string[]) => {
   let url = `https://api.zapper.fi/v2/balances?`;
   addresses.forEach((address, _index) => {
     url += `addresses[]=${address}${
       _index === addresses.length - 1 ? "" : "&"
-    }`
-  })
-  return encodeURI(url)
+    }`;
+  });
+  return encodeURI(url);
 };
 
 const generateEventSourceDict = (apiKey: string) => {
@@ -20,64 +21,99 @@ const generateEventSourceDict = (apiKey: string) => {
       "User-Agent": "Mozilla/5.0",
       Authorization: `Basic ${Buffer.from(`${apiKey}:`).toString("base64")}`,
     },
-  }
-}
+  };
+};
 
 function getBalances(eventSource: EventSource): Promise<any[]> {
   return new Promise((resolve, reject) => {
-    const result: any[] = []
+    const result: any[] = [];
 
     eventSource.addEventListener("open", () => {
-      console.log("Open ...")
+      console.log("Open ...");
     });
-    
+
     eventSource.addEventListener("error", (err) => {
-      reject(new Error(err.message))
+      reject(new Error(err.message));
     });
-    
+
     eventSource.addEventListener("balance", ({ data }) => {
       const parsedData = JSON.parse(data);
       const { appId, app, balance } = parsedData;
-      
+
       if (appId !== "nft") {
         if (appId === "tokens") {
           const { wallet } = balance;
           if (Object.keys(wallet).length > 0) {
             Object.keys(wallet).forEach((value) => {
-              result.push(wallet[value])
-              const { key, balanceUSD, context, network } = wallet[value]
-              const { symbol } = context
-              console.log(`${balanceUSD} $ of ${symbol} on ${network} wallet`)
+              result.push(wallet[value]);
+              const { key, balanceUSD, context, network } = wallet[value];
+              const { symbol } = context;
+              console.log(`${balanceUSD} $ of ${symbol} on ${network} wallet`);
             });
           }
         } else if (app.meta.total > 0) {
-          console.log(`${app.meta.total} $ deployed in ${appId}`)
+          console.log(`${app.meta.total} $ deployed in ${appId}`);
         }
       }
-    })
-    
+    });
+
     eventSource.addEventListener("end", () => {
       eventSource.close();
-      resolve(result)
-    })
-  })
+      resolve(result);
+    });
+  });
 }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const addresses = (req.query.addresses as string).split(',')
+function setupZapper(sdk: Context) {
+  async function getPortfolio(addresses: string[]) {
+    const url = generateUrl(addresses);
+    const eventSourceDict = generateEventSourceDict(process.env.ZAPPER_API_KEY);
+    const eventSource = new EventSource(url, eventSourceDict);
+    const balances = await getBalances(eventSource);
 
-  const url = generateUrl(addresses)
-  const eventSourceDict = generateEventSourceDict(process.env.ZAPPER_API_KEY)
-  const eventSource = new EventSource(url, eventSourceDict)
+    return balances;
+  }
 
-  const balances = await getBalances(eventSource)
+  sdk.register({
+    id: "zapper",
+    queries: {
+      getPortfolio: getPortfolio,
+    },
+    metadata: {},
+  });
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const addresses = (req.query.addresses as string).split(",");
+
+  const sdk = new CryptoStatsSDK({
+    mongoConnectionString: process.env.MONGO_KEY,
+  });
+
+  const zapperCollection = sdk.getCollection("zapper-collection");
+  zapperCollection.setCacheKeyResolver(
+    (id, query,addresses) => addresses + Math.floor(Date.now() / 60 / 60 / 1000)
+  );
+  zapperCollection.addAdaptersWithSetupFunction(setupZapper);
+  const adapter = zapperCollection.getAdapter("zapper");
+
+  const balances = await adapter.query("getPortfolio", addresses);
 
   if (balances) {
-    res.setHeader('Cache-Control', `max-age=${12 * 60 * 60}, s-maxage=${5 * 24 * 60 * 60}, stale-while-revalidate`)
-    res.json({ success: true, value: balances })
-    return
+    res.setHeader(
+      "Cache-Control",
+      `max-age=${12 * 60 * 60}, s-maxage=${
+        5 * 24 * 60 * 60
+      }, stale-while-revalidate`
+    );
+    res.json({ success: true, value: balances });
+    return;
   }
-  res.status(404).json({ success: false, error: `Couldn't find data on ${req.query.date}` })
+  res
+    .status(404)
+    .json({ success: false, error: `Couldn't find data on ${req.query.date}` });
+
+  res.json(balances);
 }
 
-export default wrapHandler(handler)
+export default wrapHandler(handler);
